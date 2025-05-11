ad1f50c738c9e890ba2ed026fb9f9228f2501155
jain-hemant <hemantjain1321@gmail.com>
2025-05-11 17:53:03 +0530

    Assignment

--- src/pages/AddNotes.jsx
+++ src/pages/AddNotes.jsx
@@ -1,22 +1,24 @@
-import { Box, Button, VStack, Flex } from '@chakra-ui/react'
+import { Box, Button, VStack, Flex, Input } from '@chakra-ui/react'
 import React from 'react'
 
 export default function AddNotes() {
     return (
-        <VStack>
-            <Box mx={"auto"} >
-                <form style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }} >
-                    <Flex flexDirection={"column"}>
-                        <label for="title">Title</label>
-                        <input type="text" placeholder='Enter Title...' />
+        <Flex w={"100vw"} h={"100vh"} justifyContent={"center"} alignItems={"center"}  >
+
+            <form >
+                <Flex flexDirection={"column"} mx={"auto"} gapY={"20px"} p={"30px 20px"} boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px; ">
+                    <Flex flexDirection={"column"} gapY={"10px"}>
+                        <lable for="title">Title</lable>
+                        <Input type="text" placeholder='Enter Title...' w={"md"} py={"2px"} />
                     </Flex>
-                    <Flex flexDirection={"column"}>
+                    <Flex flexDirection={"column"} gapY={"10px"}>
                         <label for="content">Content</label>
-                        <input type="text" placeholder='Enter Content...' />
+                        <Input type="text" placeholder='Enter Content...' w={"md"} />
                     </Flex>
                     <Button>Add Notes</Button>
-                </form>
-            </Box>
-        </VStack>
+                </Flex>
+            </form>
+
+        </Flex>
     )
 }
